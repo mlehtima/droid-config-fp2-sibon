22023480f095d152412c74d3310388a94b049151
Matti Lehtimäki <matti.lehtimaki@gmail.com>
2019-04-07 21:40:16 +0300

    [bluetooth] Fix Bluetooth init script for busybox grep.

--- sparse/usr/bin/droid/droid-hcismd-up.sh
+++ sparse/usr/bin/droid/droid-hcismd-up.sh
@@ -11,7 +11,7 @@ while [ ! $i -gt $MAXTRIES ]  ; do
     if [ -e /sys/class/bluetooth/hci0 ] ; then
         # found hci0, get/set BT MAC address
         echo 0 > /sys/module/hci_smd/parameters/hcismd_set
-        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oP '([0-9a-f]{2}:){5}([0-9a-f]{2})')
+        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHSL0 2>1 | grep -oE '([0-9a-f]{2}:){5}([0-9a-f]{2})')
         echo "BT MAC: $bt_mac"
         if [ ! -z "$bt_mac" ] ; then
             echo $bt_mac > /var/lib/bluetooth/board-address
